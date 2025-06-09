b5f75e5321712e1c39edab18def4c83a518f64c2
Patryk <patryk.d.szczesniak@gmail.com>
2025-06-09 13:40:50 +0200

    fix: added AuthProvider to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import AuthProvider from "./context/AuthProvider";
+import PrivateRoute from "./components/common/PrivateRoute";
 import FreeplayPage from "./pages/FreeplayPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -12,14 +14,18 @@ function App() {
     <>
       <Toaster position="top-center" />
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<FreeplayPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-        </Routes>
+        <AuthProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<FreeplayPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/quiz" element={<QuizPage />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
       </Router>
     </>
   );
