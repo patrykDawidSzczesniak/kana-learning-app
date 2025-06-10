c0c60a6d9671569eb2c1fd67fb0d2e5e6e7271b2
Patryk <patryk.d.szczesniak@gmail.com>
2025-06-10 08:28:23 +0200

    fix: made onClick prop optional

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -3,14 +3,14 @@ type ButtonType = "submit" | "button";
 interface ButtonProps {
   type: ButtonType;
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
-function Button({ label, onClick }: ButtonProps) {
+function Button({ type, label, onClick = () => null }: ButtonProps) {
   return (
     <button
       className="w-full p-3 bg-blue-500 text-white font-semibold border-none rounded-md"
-      type="submit"
+      type={type}
       onClick={onClick}
     >
       {label}
